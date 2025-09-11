a070c310cebd849249eaf8494293f26c13b9d99c
Sohaib Romee <148002469+Romee111@users.noreply.github.com>
2025-09-11 21:48:49 +0300

    add

--- techjeddah/src/app/shared/tasks/project.service.ts
+++ techjeddah/src/app/shared/tasks/project.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProjectService {
-  private taskApi = 'http://localhost:2300/service';
+  private taskApi = 'http://localhost:8000/service';
 
   constructor(private http: HttpClient) {}
 
