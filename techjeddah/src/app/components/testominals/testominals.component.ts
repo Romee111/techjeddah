8d11be8f97ad7b4dc029215df650527b0f93eb7f
Sohaib Romee <148002469+Romee111@users.noreply.github.com>
2025-09-16 16:37:14 +0300

    add

--- techjeddah/src/app/components/testominals/testominals.component.ts
+++ techjeddah/src/app/components/testominals/testominals.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { Testimonial } from 'src/app/shared/testoimonial.interface';
 
 @Component({
   selector: 'app-testominals',
   templateUrl: './testominals.component.html',
   styleUrls: ['./testominals.component.css']
 })
+
+
 export class TestominalsComponent {
+
+@Input() testimonials: Testimonial[] = [
+    {
+      authorName: 'Sohaib Romee',
+      role: 'CEO and a ITProfessional',
+      text: 'I dont chase perfection-I persue perfessionalism , because thats what builds real business.',
+      image: '../assets/CEO.jpg'
+    },
+    {
+      authorName: 'Sara Ahmed',
+      role: 'UI Designer',
+      text: 'The tutorials are clear, modern, and easy to understand!',
+      image: 'https://via.placeholder.com/100/7c3aed/ffffff'
+    },
+    {
+      authorName: 'Usman Tariq',
+      role: 'Backend Engineer',
+      text: 'I learned how to connect frontend with backend in a professional way.',
+      image: 'https://via.placeholder.com/100/2563eb/ffffff'
+    }
+  ];
+
+
+
+
 
 }
---
+++ techjeddah/src/app/shared/testoimonial.interface.ts
@@ -0,0 +1,6 @@
+export interface Testimonial {
+    authorName: string;
+    role: string;
+    text: string;
+    image: string;
+}
